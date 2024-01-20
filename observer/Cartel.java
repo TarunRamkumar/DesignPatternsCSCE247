0634144b3ed9bea71650c53b119c6c8fc1c31d38
TarunRamkumar <thepeeps33@gmail.com>
2024-01-20 16:10:12 -0500

    Began to Javadoc

--- observer/Cartel.java
+++ observer/Cartel.java
@@ -1,17 +1,29 @@
 package observer;
 
 import java.util.ArrayList;
-
+/**
+ *  A drug cartel that observes the drug cook/boss
+ * @author Tarun
+ */
 public class Cartel implements Observer{
     private Subject cook;
     private ArrayList<Sighting> sightings;
-
+    /**
+     * Creates a new Cartel object, creating a new ArrayList of sightings and registering the Cartel as an observer of the given cook.
+     * @param cook The drug cook that the Cartel will observe. 
+     */
     public Cartel(Subject cook)
     {
         this.cook = cook;
         sightings = new ArrayList<Sighting>();
         cook.registerObserver(this);
     }
+    /**
+     * Updates the Cartel on a sighting of the cook.
+     * @param location String representation of the location of the cook
+     * @param description String description of the sighting of the cook
+     * @param accomplices ArrayList of String representations of accomplice names
+     */
     @Override
     public void update(String location, String description, ArrayList<String> accomplices) {
         // TODO Auto-generated method stub
@@ -19,6 +31,10 @@ public class Cartel implements Observer{
     }
 
     @Override
+    /**
+     * Returns a String that contains all the sightings the Cartel has of the Cook
+     * @return A string representation of the list of sightings of the cook
+     */
     public String getLog() {
         // TODO Auto-generated method stub
         String log = new String();
--- observer/Cook.java
+++ observer/Cook.java
@@ -2,11 +2,17 @@ package observer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
+/**
+ *  A drug cook
+ * @author Tarun
+ */
 public class Cook implements Subject{
     private ArrayList<Observer> observers;
     private String name;
-    
+    /**
+     * Creates a new drug cook
+     * @param name The name of the cook
+     */
     public Cook(String name)
     {
         if(name != null)
@@ -17,6 +23,13 @@ public class Cook implements Subject{
     }
 
     @Override
+    /**
+     * Notifies the registered observers of the cook of the cook's location, description of action, and accomplices.
+     * @param location String representation of the cook's location
+     * @param description String description of the cook's action
+     * @param accomplices ArrayList of String names of accomplices
+     *
+     */
     public void notifyObservers(String location, String description, ArrayList<String> accomplices) {
         for(Observer o : observers)
         {
@@ -25,6 +38,10 @@ public class Cook implements Subject{
     }
 
     @Override
+    /**
+     * Adds new observers of the cook
+     * @param observer The observer object to be added/registered
+     */
     public void registerObserver(Observer observer) {
         // TODO Auto-generated method stub
         observers.add(observer);
@@ -32,16 +49,28 @@ public class Cook implements Subject{
     }
 
     @Override
+    /**
+     * Removes the given observer from the cook's list of observers
+     * @param observer The observer object to be removed
+     */
     public void removeObserver(Observer observer) {
         // TODO Auto-generated method stub
         observers.remove(observer);
     }
-
+    /**
+     * Notifies registered observers of the cook's location, description of action, and accomplices they were with 
+     * @param location String representation of the cook's location
+     * @param description String description of the cook's action
+     * @param accomplices String that lists names of accomplices with the cook 
+     */
     public void enterSighting(String location, String description, String accomplices)
     {
-        this.notifyObservers(location, description, new ArrayList<>(Arrays.asList(accomplices.split(", "))));
+        notifyObservers(location, description, new ArrayList<>(Arrays.asList(accomplices.split(", "))));
     }
-
+    /**
+     * The name of the cook
+     * @return A string representation of the cook's name. 
+     */
     public String getName()
     {
         return this.name;

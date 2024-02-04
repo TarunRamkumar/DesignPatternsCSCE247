1c93df91ddf4ab2b9eeb341dd4b1095031ab0b89
TarunRamkumar <thepeeps33@gmail.com>
2024-02-03 20:14:23 -0500

    Finished Javadocing

--- spelling/State.java
+++ spelling/State.java
@@ -1,23 +1,35 @@
 package spelling;
 import java.util.ArrayList;
 import java.util.Random;
+/**
+ * The state of the spellingList
+ * @author Tarun
+ */
 public abstract class State {
     protected SpellingList spellingList;
     protected ArrayList<String> words;
     private Random rand;
-
+    /**
+     * Creates a new state
+     * @param spellingList The spellingList the state should be applied to
+     */
     public State(SpellingList spellingList)
     {
         this.spellingList = spellingList;
         words = new ArrayList<String>();
         rand = new Random();
     }
-
+    /**
+     * Randomly generates a word from the word list
+     * @return A random string word
+     */
     public String getNextWord()
     {
         return words.get(rand.nextInt(words.size()));
     }
-
+    /**
+     * Increases/Decreases the grades in the children
+     */
     public abstract void increaseGrade();
     public abstract void decreaseGrade();
     
--- spelling/ThirdGradeState.java
+++ spelling/ThirdGradeState.java
@@ -1,18 +1,28 @@
 package spelling;
-
+/**
+ * The program's hardest state
+ * @author Tarun
+ */
 public class ThirdGradeState extends State {
-    
+    /**
+     * Creates a new ThirdGradeState
+     * @param spellingList The spellingList to apply the state to
+     */
     public ThirdGradeState(SpellingList spellingList)
     {
         super(spellingList);
         this.words = FileReader.getWords("spelling\\third.txt");
     }
-
+    /**
+     * Prints an error message as this is the highest grade 
+     */
     public void increaseGrade()
     {
         System.out.println("Sorry, you are already at the highest level. Cannot increase further.");
     }
-
+    /**
+     * Sets the state of the spellingList to the secondGrade
+     */
     public void decreaseGrade()
     {
         spellingList.setState(spellingList.getSecondGradeState());
